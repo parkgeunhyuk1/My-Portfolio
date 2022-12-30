b3c2866d82a871658a1f2eaa5a70872ccd6c6f23
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-12-30 16:39:41 +0900

    [Fix]: work refix

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,61 +1,59 @@
 export const Work = [
-    {
-        id:1,
-        name:"Weather app",
-        description:"This is my first project using typescript which show weather condition through user's location",
-        tags:["typescript"],
-        
-        demo:"https://golden-strudel-362da8.netlify.app",
-        github:"https://github.com/parkgeunhyuk1/weather-typescript"
-    },
-    {
-        id:2,
-        name:"Movie information site",
-        description:"this is a site which show movie's information",
-        tags:["react","redux"],
-        
-        demo:"https://enchanting-semifreddo-b546d3.netlify.app",
-        github:"https://github.com/parkgeunhyuk1/parkflix"
-        
-    },
-    {
-        id:3,
-        name:"Chatting App",
-        description:"this is a site which user can commuicate each other and make communication room. I make Ui and server.",
-        tags:["Typescript","MongoDB","Node.js",'Socket.io'],
-        
-        demo:"/",
-        github:"https://github.com/parkgeunhyuk1/chat-typescript"
-    },
-    {
-        id:4,
-        name:"Portfolio Web Service",
-        description:"user can use this site to upgrade their portfolio. user can upload own information, and can see other's. and can share to other people",
-        tags:["react","bootstrap","react-router"],
-        
-        demo:"/",
-        github:"https://github.com/5owl5/portfolio-frontend"
-       
-    },{
-        id:5,
-        name:"Challenge for nature",
-        description:"A site that encourages people to protect the environment. We encourage users using a point system ",
-        tags:["react",'rechart',"framer-motion"],
-        
-        demo:"/",
-        github:"https://github.com/HaEunCho248/tt-web-service"
-        
-    },
-    {
-        id:6,
-        name:"Responsive Navigation Menu",
-        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-        tags:["react","react-router","css"],
-        
-        demo:"/",
-        github:"https://github.com/codebucks27/React-responsive-navbar"
-        
-    },
-    
-  
-] 
+  {
+    id: 1,
+    name: "CHAIR COACH",
+    description:
+      "This is a project which user can do stretching with AI's coaching. Also, user can inspect whether their's neck is in Forward Head Posture or not",
+    tags: [
+      "typescript",
+      "socket.io",
+      "tensorflow.js",
+      "styled-component",
+      "react",
+      "recoil",
+    ],
+
+    demo: "https://chaircoach.dogfoot.info/",
+    github: "https://github.com/zzzzsb/ChairCoach",
+  },
+  {
+    id: 2,
+    name: "Challenge for nature",
+    description:
+      "A site that encourages people to protect the environment. User can communicate each other thorugh chatting system of this site. ",
+    tags: ["react", "rechart", "socket.io"],
+
+    demo: "https://youtu.be/OHws7qVZKNQ",
+    github: "https://github.com/HaEunCho248/tt-web-service",
+  },
+  {
+    id: 3,
+    name: "Chatting App",
+    description:
+      "this is a site which user can commuicate each other and make communication room. I make Ui and server.",
+    tags: ["Typescript", "MongoDB", "Node.js", "Socket.io", "Redux", "react"],
+
+    demo: "/",
+    github: "https://github.com/parkgeunhyuk1/chat-typescript",
+  },
+  {
+    id: 4,
+    name: "Movie information site",
+    description: "this is a site which show movie's information",
+    tags: ["react", "redux"],
+
+    demo: "https://enchanting-semifreddo-b546d3.netlify.app",
+    github: "https://github.com/parkgeunhyuk1/parkflix",
+  },
+
+  {
+    id: 5,
+    name: "Portfolio Web Service",
+    description:
+      "user can use this site to upgrade their portfolio. user can upload own information, and can see other's. and can share to other people",
+    tags: ["react", "bootstrap", "react-router"],
+
+    demo: "/",
+    github: "https://github.com/5owl5/portfolio-frontend",
+  },
+];
