0ff816c4e15bf6b9fe69bd78fd06dc159eebb6c9
parkgeunhyuk1 <108388408+parkgeunhyuk1@users.noreply.github.com>
2022-12-30 17:12:24 +0900

    [Fix]: logo click to go homepage

--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-import { DarkTheme } from '../components/Themes'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { DarkTheme } from "../components/Themes";
 
-
-const Logo=styled.h1`
-display: inline-block;
-color: ${props => props.color=== 'dark' ? DarkTheme.text : DarkTheme.body};
-font-family: 'Pacifico',cursive;
-position: fixed;
-left: 2rem;
-top: 2rem;
-z-index:3;
-`
-
+const Logo = styled.h1`
+  display: inline-block;
+  color: ${(props) =>
+    props.color === "dark" ? DarkTheme.text : DarkTheme.body};
+  font-family: "Pacifico", cursive;
+  position: fixed;
+  left: 2rem;
+  top: 2rem;
+  z-index: 3;
+`;
 
 const LogoComponent = (props) => {
+  const navigate = useNavigate();
   return (
-    <Logo color={props.theme}>
+    <Logo
+      onClick={() => {
+        navigate("/");
+      }}
+      color={props.theme}
+    >
       PKH
     </Logo>
-  )
-}
+  );
+};
 
-export default LogoComponent
+export default LogoComponent;
